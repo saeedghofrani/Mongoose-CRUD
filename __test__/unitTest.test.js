7623acfee6cd16f85a7989a9583b67915fad89cb
Saeed <sa.ghofraniivari@gmail.com>
2022-04-18 11:10:44 +0430

    unit test

--- __test__/unitTest.test.js
+++ __test__/unitTest.test.js
@@ -1,12 +1,33 @@
-const getCompany = require('../controller/company.controller')._get;
+const createCompany = require('../services/createcompany.service');
+const db = require('./db');
+// jest.setTimeout(30000);
 
-describe('Test Handlers', function () {
+// beforeAll(async () => {
+//     await db.connect()
+// });
+
+// afterEach(async () => {
+//     await db.clearDatabase()
+// });
+
+// afterAll(async () => {
+//     await db.closeDatabase()
+// });
+beforeAll(async () => await db.connect());
+
+afterEach(async () => await db.clearDatabase());
+
+afterAll(async () => await db.closeDatabase());
+
+// require("../db/connection.db");
 
 
-    test('should get all companies', async () => {
-        await getCompany();
-        expect(res.status).toHaveBeenCalledWith(200);
-        expect(res.json).toHaveBeenCalledWith({ username: 'hugo' });
+describe('company will created when', function () {
+
+    test('First company', async () => {
+        const company = await createCompany('lalallal0', '12444444444');
+        expect(company.company_name).toEqual('lalallal0');
+        expect(company.value).toEqual('12444444444');
     });
 
 });
